7d07ccc4cea99b5b5989eb44f6eced5610a909dc
sinooo2001 <sina098ali@gmail.com>
2023-03-03 00:00:13 +0300

    first commit

---
+++ application_widget.dart
@@ -0,0 +1,16 @@
+void main() {
+  runapp
+  runApp;(Task_hom());
+}
+
+class werk_ecx6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "",
+        home: Scaffold(
+          appBar: AppBar(),
+          drawer: Drawer(),
+        ),);
+  }
+}
